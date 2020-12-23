5c5ea6d8b0fb6671f175125699f2a287b0343783
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-23 10:27:29 +0100

    fix production.py

--- settings/production.py
+++ settings/production.py
@@ -1,7 +1,7 @@
 from os import environ
 from .development import *
 
-DEBUG = False
-ENV = "production"
+DEBUG = environ.get("DEBUG")
+ENV = environ.get("ENV")
 SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
 SECRET_KEY = environ.get("SECRET_KEY")
