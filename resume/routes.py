8ffa8278d1dd1e4d02486564275fe19a5b8e0f13
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-23 08:41:59 +0100

    updated routes.py

--- resume/routes.py
+++ resume/routes.py
@@ -1,6 +1,35 @@
-from flask import current_app as app, render_template
+import os
+import secrets
+
+from flask import (
+    current_app as app,
+    render_template,
+    jsonify,
+    json,
+    request,
+    send_from_directory,
+    url_for,
+)
+from weasyprint import HTML
+
+DIRECTORY = app.config.get("PDF_PATH")
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    data = json.loads(request.data)
+    html = render_template("resumes/1.html", data=data)
+    filename = secrets.token_hex(10) + ".pdf"
+    pdf = HTML(string=html).write_pdf(os.path.join(DIRECTORY, filename))
+
+    return jsonify({"url": url_for("send_pdf", name=filename, _external=True)})
+
+
+@app.route("/pdf/<name>")
+def send_pdf(name):
+    return send_from_directory(DIRECTORY, name)
