afea1c54c1ea1ac83fc711d039b70023ff7114c4
prettyirrelevant <ienioladewumi@gmail.com>
2020-12-23 10:34:59 +0100

    fixed models.py

--- resume/models.py
+++ resume/models.py
@@ -7,5 +7,5 @@ class Download(db.Model):
     __tablename__ = "downloads"
 
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(15), nullable=False)
+    filename = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=func.now())
